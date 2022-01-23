d22d3dd4e1f211c1d122c3ba16cd6804d5e4a6cf
ss2299 <ss2299@hotmail.com>
2022-01-23 19:42:04 +0800

    Camera input

---
+++ camera_in.py
@@ -0,0 +1,23 @@
+import sys
+import cv2
+
+cap = cv2.VideoCapture('Video/vtest.avi')
+
+if not cap.isOpened():
+    print('camera open failed')
+    sys.exit()
+    
+
+while True:
+    ret, frame = cap.read()
+
+    
+    
+    if not ret:
+        break
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) == 27:
+        break
+
+cap.release()
